e4649ecb1cb61a254ca905c4c898d3a509e7eac5
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-01-21 21:40:33 -0300

    api project

---
+++ DragonsApp.WebAPI/Program.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// get configs
+
+var connectionString = builder.Configuration.GetConnectionString("dragons");
+
+// Add services to the container.
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// app services
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
